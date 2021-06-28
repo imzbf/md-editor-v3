b1199af50812dbd19e62b0e3f1b1c286a48a866b
zhoubangfu <zbfcqtl@163.com>
2021-06-28 22:32:45 +0800

    feat: toolbar样式修改

--- src/MdEditor/components/Toolbar.tsx
+++ src/MdEditor/components/Toolbar.tsx
@@ -8,117 +8,123 @@ export default defineComponent({
     return () => (
       <div class={`${prefix}-toolbar`}>
         <div class={`${prefix}-toolbar-left`}>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="加粗">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-bold"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="下划线">
+            <svg class={`${prefix}-icon`} aria-hidden="true">
+              <use xlinkHref="#icon-underline"></use>
+            </svg>
+          </div>
+          <div class={`${prefix}-toolbar-item`} title="斜体">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-italic"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="删除线">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-xiahuaxian"></use>
+              <use xlinkHref="#icon-strike-through"></use>
             </svg>
           </div>
           <Divider />
-          <div>
-            <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-strikethrough"></use>
-            </svg>
-          </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="标题">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-title"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="引用">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-baojiaquotation2"></use>
+              <use xlinkHref="#icon-quote"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="无序列表">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-unorderedList"></use>
+              <use xlinkHref="#icon-unordered-list"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="有序列表">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-orderedList"></use>
+              <use xlinkHref="#icon-ordered-list"></use>
             </svg>
           </div>
-
-          <div>
+          <Divider />
+          <div class={`${prefix}-toolbar-item`} title="行内代码">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-daima"></use>
+              <use xlinkHref="#icon-code-row"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="代码块">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-daima1"></use>
+              <use xlinkHref="#icon-code"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="链接">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-link"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="图片">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-image"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="表格">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-table"></use>
             </svg>
           </div>
-          <div>
+          <Divider />
+          <div class={`${prefix}-toolbar-item`} title="撤回">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-chexiao"></use>
+              <use xlinkHref="#icon-revoke"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="取消撤回">
+            <svg class={`${prefix}-icon`} aria-hidden="true">
+              <use xlinkHref="#icon-next"></use>
+            </svg>
+          </div>
+          <div class={`${prefix}-toolbar-item`} title="保存">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-baocun"></use>
             </svg>
           </div>
         </div>
-        <ul class={`${prefix}-toolbar-right`}>
-          <div>
+        <div class={`${prefix}-toolbar-right`}>
+          <div class={`${prefix}-toolbar-item`} title="浏览器内全屏">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-fangda"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="全屏放大">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-fullScreen"></use>
             </svg>
           </div>
 
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="分栏">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-subColumn"></use>
+              <use xlinkHref="#icon-sub-column"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="目录">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-mulu"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="帮助">
             <svg class={`${prefix}-icon`} aria-hidden="true">
-              <use xlinkHref="#icon-bangzhu"></use>
+              <use xlinkHref="#icon-help"></use>
             </svg>
           </div>
-          <div>
+          <div class={`${prefix}-toolbar-item`} title="源码">
             <svg class={`${prefix}-icon`} aria-hidden="true">
               <use xlinkHref="#icon-github"></use>
             </svg>
           </div>
-        </ul>
+        </div>
       </div>
     );
   }
