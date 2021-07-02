7963028d67b7fe00957c193bf2aa058bc6bb7278
zhoubangfu <zbfcqtl@163.com>
2021-07-02 16:25:00 +0800

    feat: md转义，代码样式

--- MdEditor/layouts/Content/index.tsx
+++ MdEditor/layouts/Content/index.tsx
@@ -1,23 +1,51 @@
-import { defineComponent, ref, computed } from 'vue';
+import { defineComponent, ref, computed, onMounted, Teleport, inject } from 'vue';
 import { prefix } from '../../Editor';
+import marked from 'marked';
+
+declare global {
+  interface Window {
+    hljs: any;
+  }
+}
 
 export default defineComponent({
   name: 'MDEditorContent',
   setup() {
-    const content = ref<string>('');
+    const highlight = inject('highlight') as { js: string; css: string };
+    const text = ref<string>('');
+
+    const html = computed((): string => {
+      return marked(text.value);
+    });
+
+    onMounted(() => {
+      window.addEventListener('load', () => {
+        marked.setOptions({
+          highlight(code) {
+            return window.hljs.highlightAuto(code).value;
+          }
+        });
+      });
+    });
 
     return () => (
-      <div class={`${prefix}-content`}>
-        <div class={[`${prefix}-input-wrapper`]}>
-          <textarea
-            value={content.value}
-            onInput={({ target }: Event) =>
-              (content.value = (target as HTMLTextAreaElement).value)
-            }
-          />
+      <>
+        <div class={`${prefix}-content`}>
+          <div class={[`${prefix}-input-wrapper`]}>
+            <textarea
+              value={text.value}
+              onInput={({ target }: Event) =>
+                (text.value = (target as HTMLTextAreaElement).value)
+              }
+            />
+          </div>
+          <div class={`${prefix}-preview-wrapper`} innerHTML={html.value}></div>
         </div>
-        <div class={`${prefix}-preview-wrapper`} innerHTML={content.value}></div>
-      </div>
+        <Teleport to={document.head}>
+          <link rel="stylesheet" href={highlight.css} />
+          <script src={highlight.js} />
+        </Teleport>
+      </>
     );
   }
 });
