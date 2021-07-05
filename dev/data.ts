22e8752b093f69226a6f8b108035af7dead21bcf
zhoubangfu <zbfcqtl@163.com>
2021-07-05 11:38:24 +0800

    fix: 首次md编译调整及highlight接入调整

--- dev/data.ts
+++ dev/data.ts
@@ -1,2 +1,2 @@
 export const mdText =
-  '## 二级标题 \n### 三级标题 \n\n## 代码演示 \n\n```css\npre {\n  position: relative;\n  margin: 20px 10px;\n  border-radius: 5px;\n  box-shadow: #00000055 0px 2px 2px;\n}\n```';
+  '## 二级标题 \n### 三级标题 \n\n## 代码演示 \n\n  ```js\nconst data = Object.create({a: 1, b: 2});\n\nconsole.log(data.a);\n```';
