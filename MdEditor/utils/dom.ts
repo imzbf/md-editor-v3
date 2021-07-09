3b45c297dee1ad915e91915d84ccba44c545851f
zhoubangfu <zbfcqtl@163.com>
2021-07-09 15:14:47 +0800

    feat: 定制的移动元素方法

--- MdEditor/utils/dom.ts
+++ MdEditor/utils/dom.ts
@@ -1,19 +1,25 @@
-function keyMove(target: HTMLElement) {
-  target.onmousedown = (mdown: MouseEvent) => {
-    const parent: HTMLElement = target.parentElement || document.body;
-    // 模块
+/**
+ * 设置页面元素可移动
+ *
+ * @param trigger 触发器，通过该元素移动父级元素
+ */
+export const keyMove = (
+  trigger: HTMLElement,
+  moveHandler?: (left: number, top: number) => void
+): (() => void) => {
+  const triggerMouseDown = (mdown: MouseEvent) => {
+    const parent: HTMLElement = trigger.parentElement || document.body;
+    // 被移动框大小
     const width = parent.offsetWidth;
     const height = parent.offsetHeight;
     // 当前页长宽
     const clientWidth = document.documentElement.clientWidth;
     const clientHeight = document.documentElement.clientHeight;
 
-    //
     const x = mdown.offsetX;
     const y = mdown.offsetY;
 
-    //
-    document.onmousemove = (e: MouseEvent) => {
+    const mouseMoveHandler = (e: MouseEvent) => {
       let tx =
         (e.pageX || e.clientX + document.body.scrollLeft - document.body.clientLeft) - x;
       let ty =
@@ -21,15 +27,27 @@ function keyMove(target: HTMLElement) {
       tx = tx < 1 ? 1 : tx < clientWidth - width - 1 ? tx : clientWidth - width - 1;
       ty = ty < 1 ? 1 : ty < clientHeight - height - 1 ? ty : clientHeight - height - 1;
 
-      parent.style.left = tx + 'px';
-      parent.style.top = ty + 'px';
+      if (moveHandler) {
+        moveHandler(tx, ty);
+      } else {
+        parent.style.left = tx + 'px';
+        parent.style.top = ty + 'px';
+      }
     };
 
-    document.onmouseup = function () {
-      document.onmousemove = null;
-      document.onmouseup = null;
+    document.addEventListener('mousemove', mouseMoveHandler);
+
+    const mouseUpHandler = () => {
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      trigger.removeEventListener('mouseup', mouseUpHandler);
     };
+    trigger.addEventListener('mouseup', mouseUpHandler);
   };
-}
 
-export default keyMove;
+  trigger.addEventListener('mousedown', triggerMouseDown);
+
+  return () => {
+    // 清除时间监听
+    trigger.removeEventListener('mousedown', triggerMouseDown);
+  };
+};
