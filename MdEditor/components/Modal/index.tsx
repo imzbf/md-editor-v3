a6bae477f1d3f8a356c6612c75dfe8fc40a7a0cb
zhoubangfu <zbfcqtl@163.com>
2021-07-09 15:15:41 +0800

    feat: 弹窗v1.0

---
+++ MdEditor/components/Modal/index.tsx
@@ -0,0 +1,129 @@
+import {
+  defineComponent,
+  PropType,
+  Teleport,
+  ref,
+  onMounted,
+  onUnmounted,
+  reactive,
+  watch,
+  nextTick
+} from 'vue';
+import { prefix } from '../../Editor';
+import { getSlot } from '../../utils/vue-tsx';
+import { keyMove } from '../../utils/dom';
+import './style.less';
+
+export type ModalProps = Readonly<{
+  title?: string;
+  visible?: boolean;
+  onClosed?: (visible: boolean) => void;
+}>;
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String as PropType<string>,
+      default: ''
+    },
+    visible: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    onClosed: {
+      type: Function as PropType<() => void>,
+      default: () => () => {}
+    }
+  },
+  setup(props, ctx) {
+    const modalVisible = ref(props.visible);
+
+    const modalClass = ref([`${prefix}-modal`]);
+
+    const modalRef = ref();
+    const modalHeaderRef = ref();
+
+    // 移动元素方法返回清除监听事件方法。
+    let keyMoveClear = () => {};
+
+    const initPos = reactive({
+      left: '0px',
+      top: '0px'
+    });
+
+    onMounted(() => {
+      keyMoveClear = keyMove(modalHeaderRef.value, (left: number, top: number) => {
+        initPos.left = left + 'px';
+        initPos.top = top + 'px';
+      });
+    });
+
+    onUnmounted(() => {
+      keyMoveClear();
+    });
+
+    watch(
+      () => props.visible,
+      (nVal) => {
+        if (nVal) {
+          modalClass.value.push('zoom-in');
+          modalVisible.value = nVal;
+
+          nextTick(() => {
+            const halfWidth = (modalRef.value as HTMLElement).offsetWidth / 2;
+            const halfHeight = (modalRef.value as HTMLElement).offsetHeight / 2;
+
+            const halfClientWidth = document.documentElement.clientWidth / 2;
+            const halfClientHeight = document.documentElement.clientHeight / 2;
+
+            initPos.left = halfClientWidth - halfWidth + 'px';
+            initPos.top = halfClientHeight - halfHeight + 'px';
+          });
+
+          setTimeout(() => {
+            modalClass.value = modalClass.value.filter((item) => item !== 'zoom-in');
+          }, 140);
+        } else {
+          modalClass.value.push('zoom-out');
+          setTimeout(() => {
+            modalClass.value = modalClass.value.filter((item) => item !== 'zoom-out');
+            modalVisible.value = nVal;
+          }, 130);
+        }
+      }
+    );
+
+    return () => {
+      const slotDefault = getSlot({ ctx });
+      const slotTitle = getSlot({ props, ctx }, 'title');
+
+      return (
+        <Teleport to={document.body}>
+          <div style={{ display: modalVisible.value ? 'block' : 'none' }}>
+            <div class={`${prefix}-modal-mask`} />
+            <div class={`${prefix}-modal-wrapper`}>
+              <div
+                class={modalClass.value}
+                style={{ left: initPos.left, top: initPos.top }}
+                ref={modalRef}
+              >
+                <div class={`${prefix}-modal-header`} ref={modalHeaderRef}>
+                  <div class={`${prefix}-modal-title`}>{slotTitle || ''}</div>
+                  <div
+                    class={`${prefix}-modal-close`}
+                    onClick={() => {
+                      props.onClosed && props.onClosed();
+                    }}
+                  >
+                    X
+                  </div>
+                </div>
+                <div class={`${prefix}-modal-body`}>{slotDefault}</div>
+              </div>
+            </div>
+          </div>
+        </Teleport>
+      );
+    };
+  }
+});
