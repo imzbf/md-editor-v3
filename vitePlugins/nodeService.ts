946cedcdcec16d28393c07069195870854e0826e
zhoubangfu <zbfcqtl@163.com>
2021-07-13 13:59:21 +0800

    feat: 保存上传图片

--- vitePlugins/nodeService.ts
+++ vitePlugins/nodeService.ts
@@ -1,11 +1,26 @@
 import { Plugin, ViteDevServer } from 'vite';
+import multiparty from 'multiparty';
+import fs from 'fs';
+import path from 'path';
+
+const LOCAL_IMG_PATH = path.resolve(__dirname, '../temp.local');
 
 export default (): Plugin => {
   return {
     name: 'node-service',
     configureServer: (server: ViteDevServer) => {
-      server.middlewares.use((req, res, next) => {
+      server.middlewares.use(async (req, res, next) => {
         if (/^\/api\/img\/upload$/.test(req.url)) {
+          if (!fs.existsSync(LOCAL_IMG_PATH)) {
+            fs.mkdirSync(LOCAL_IMG_PATH);
+          }
+
+          const form = new multiparty.Form({
+            uploadDir: LOCAL_IMG_PATH
+          });
+
+          form.parse(req, (_, __, files) => {});
+
           res.end(
             JSON.stringify({
               code: 0,
