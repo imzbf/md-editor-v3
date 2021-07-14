ef7fef192a96e7f58d3524ae3a3593b746c5706e
zhoubangfu <zbfcqtl@163.com>
2021-07-14 11:33:15 +0800

    feat: 支持修改语言，默认内置中英文，可自行扩展其他语言，同时可覆盖中英文

--- MdEditor/config.ts
+++ MdEditor/config.ts
@@ -2,3 +2,146 @@ export default {
   iconScriptId: 'md-editor-v3',
   iconfontUrl: '//at.alicdn.com/t/font_2605852_rqzwhnyoro9.js'
 };
+
+export interface StaticTextDefaultValue {
+  toolbarTips?: {
+    bold?: string;
+    underline?: string;
+    italic?: string;
+    strikeThrough?: string;
+    title?: string;
+    sub?: string;
+    sup?: string;
+    quote?: string;
+    unorderedList?: string;
+    orderedList?: string;
+    codeRow?: string;
+    code?: string;
+    link?: string;
+    image?: string;
+    table?: string;
+    revoke?: string;
+    next?: string;
+    save?: string;
+    pageFullscreen?: string;
+    fullscreen?: string;
+    preview?: string;
+    htmlPreview?: string;
+    github?: string;
+  };
+  titleItem?: {
+    h1?: string;
+    h2?: string;
+    h3?: string;
+    h4?: string;
+    h5?: string;
+    h6?: string;
+  };
+  linkModalTips?: {
+    title?: string;
+    descLable?: string;
+    descLablePlaceHolder?: string;
+    urlLable?: string;
+    UrlLablePlaceHolder?: string;
+    buttonOK?: string;
+    buttonUpload?: string;
+  };
+}
+
+export interface StaticTextDefault {
+  'zh-CN': StaticTextDefaultValue;
+  'en-US': StaticTextDefaultValue;
+}
+
+export type StaticTextDefaultKey = keyof StaticTextDefault;
+
+export const staticTextDefault: StaticTextDefault = {
+  'zh-CN': {
+    toolbarTips: {
+      bold: '加粗',
+      underline: '下划线',
+      italic: '斜体',
+      strikeThrough: '删除线',
+      title: '标题',
+      sub: '下标',
+      sup: '上标',
+      quote: '引用',
+      unorderedList: '无序列表',
+      orderedList: '有序列表',
+      codeRow: '行内代码',
+      code: '会计代码',
+      link: '链接',
+      image: '图片',
+      table: '表格',
+      revoke: '后退',
+      next: '前进',
+      save: '保存',
+      pageFullscreen: '浏览器全屏',
+      fullscreen: '屏幕全屏',
+      preview: '预览',
+      htmlPreview: 'html代码预览',
+      github: '源码地址'
+    },
+    titleItem: {
+      h1: '一级标题',
+      h2: '二级标题',
+      h3: '三级标题',
+      h4: '四级标题',
+      h5: '五级标题',
+      h6: '六级标题'
+    },
+    linkModalTips: {
+      title: '添加',
+      descLable: '链接描述：',
+      descLablePlaceHolder: '请输入描述...',
+      urlLable: '链接地址：',
+      UrlLablePlaceHolder: '请输入链接...',
+      buttonOK: '确定',
+      buttonUpload: '上传'
+    }
+  },
+  'en-US': {
+    toolbarTips: {
+      bold: 'bold',
+      underline: 'underline',
+      italic: 'italic',
+      strikeThrough: 'strikeThrough',
+      title: 'title',
+      sub: 'subscript',
+      sup: 'superscript',
+      quote: 'quote',
+      unorderedList: 'unordered list',
+      orderedList: 'ordered list',
+      codeRow: 'inline code',
+      code: 'block-level code',
+      link: 'link',
+      image: 'image',
+      table: 'table',
+      revoke: 'revoke',
+      next: 'undo revoke',
+      save: 'save',
+      pageFullscreen: 'fullscreen in page',
+      fullscreen: 'fullscreen',
+      preview: 'preview',
+      htmlPreview: 'html preview',
+      github: 'source code'
+    },
+    titleItem: {
+      h1: 'lv1 heading',
+      h2: 'lv2 heading',
+      h3: 'lv3 heading',
+      h4: 'lv4 heading',
+      h5: 'lv5 heading',
+      h6: 'lv6 heading'
+    },
+    linkModalTips: {
+      title: 'add ',
+      descLable: 'desc:',
+      descLablePlaceHolder: 'enter a description...',
+      urlLable: 'link:',
+      UrlLablePlaceHolder: 'enter a link...',
+      buttonOK: 'ok',
+      buttonUpload: 'upload'
+    }
+  }
+};
