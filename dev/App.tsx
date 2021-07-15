d6ffdb7e93c25343ebb07310d27fcf9d0bb42782
zhoubangfu <zbfcqtl@163.com>
2021-07-15 22:09:10 +0800

    feat: website preview

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,30 +1,18 @@
-import { defineComponent, reactive } from 'vue';
-import Editor from '../MdEditor';
-import { mdText } from './data';
-import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css';
-
+import { defineComponent, ref } from 'vue';
+import Header from './Header';
+import Preview from './Preview';
 import './style.less';
+
+export type Theme = 'dark' | 'light';
 
 export default defineComponent({
   setup() {
-    const md = reactive({
-      text: mdText
-    });
+    const theme = ref<Theme>('light');
+
     return () => (
-      <div class="container">
-        <Editor
-          hljs={hljs}
-          modelValue={md.text}
-          onChange={(value) => (md.text = value)}
-          onUploadImg={(files, callback) => {
-            console.log(files);
-            callback([
-              'https://art-1252753142.cos.ap-chengdu.myqcloud.com/2021/06301522413082599421018280471.png',
-              'https://art-1252753142.cos.ap-chengdu.myqcloud.com/2021/06301606102817061001806835437.gif'
-            ]);
-          }}
-        />
+      <div class={['doc', theme.value === 'dark' && 'theme-dark']}>
+        <Header />
+        <Preview theme={theme.value} onChange={(v: Theme) => (theme.value = v)} />
       </div>
     );
   }
