7138236e0e412c004acb1eec9b4c6e756b6f44db
zhoubangfu <zbfcqtl@163.com>
2021-07-20 09:54:08 +0800

    feat: 传统方式开发Demo调整，新增复制代码功能

--- dev/Doc/index.tsx
+++ dev/Doc/index.tsx
@@ -1,10 +1,29 @@
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 import './index.less';
-
+import copy from 'copy-to-clipboard';
 import usage from './data/usage';
 
 export default defineComponent({
   setup() {
+    const initCopyEntry = () => {
+      document.querySelectorAll('.doc-usage pre ').forEach((pre: Element) => {
+        const copyButton = pre.querySelector('.copy-button') as HTMLSpanElement;
+
+        copyButton.addEventListener('click', () => {
+          copy((pre.querySelector('code') as HTMLElement).innerText);
+
+          copyButton.innerText = '已复制！';
+          setTimeout(() => {
+            copyButton.innerText = '复制代码';
+          }, 1500);
+        });
+      });
+    };
+
+    onMounted(() => {
+      initCopyEntry();
+    });
+
     return () => (
       <div class="doc">
         <div class="container">
