b4023e22cddb3a1778ca5ad6806582bb09d4cad2
zbf <zbfcqtl@gmail.com>
2021-07-30 16:29:13 +0800

    feat: 图片裁剪上传功能

--- MdEditor/layouts/Modals/index.tsx
+++ MdEditor/layouts/Modals/index.tsx
@@ -1,18 +1,7 @@
-import {
-  computed,
-  defineComponent,
-  inject,
-  nextTick,
-  PropType,
-  reactive,
-  ref,
-  watch
-} from 'vue';
-import Modal from '../../components/Modal';
-import bus from '../../utils/event-bus';
+import { defineComponent, PropType, reactive, watch } from 'vue';
 
-import { prefix } from '../../Editor';
-import { StaticTextDefaultValue } from '../../Editor';
+import LinkModal from './Link';
+import ClipModal from './Clip';
 
 // 链接弹窗\图片弹窗\帮助弹窗
 export default defineComponent({
@@ -40,124 +29,43 @@ export default defineComponent({
   },
 
   setup(props) {
-    const ult = inject('usedLanguageText') as StaticTextDefaultValue;
-    const editorName = inject('editorName');
-
-    const title = computed(() => {
-      switch (props.type) {
-        case 'link': {
-          return `${ult.linkModalTips?.title}${ult.toolbarTips?.link}`;
-        }
-        case 'image': {
-          return `${ult.linkModalTips?.title}${ult.toolbarTips?.image}`;
-        }
-        default: {
-          return '';
-        }
-      }
+    const modelVisible = reactive({
+      link: false,
+      clip: false
     });
 
-    // 链接
-    const linkData = reactive({
-      desc: '',
-      url: ''
-    });
-
-    // 上传控件
-    const uploadRef = ref();
-
+    // 跟随外层状态
     watch(
-      () => props.type,
-      (nValue) => {
-        if (nValue === 'image') {
-          nextTick(() => {
-            (uploadRef.value as HTMLInputElement).addEventListener('change', () => {
-              bus.emit(
-                'uploadImage',
-                (uploadRef.value as HTMLInputElement).files,
-                props.onOk
-              );
-            });
-          });
+      () => props.visible,
+      (nVal) => {
+        modelVisible.link = nVal;
+
+        // 关闭时同步关闭裁剪弹窗
+        if (!nVal) {
+          modelVisible.clip = nVal;
         }
       }
     );
 
     return () => (
-      <Modal
-        title={title.value}
-        visible={props.visible}
-        onClosed={props.onCancel}
-        to={props.to}
-      >
-        {props.type === 'help' ? (
-          <div></div>
-        ) : (
-          <>
-            <div class={`${prefix}-form-item`}>
-              <label class={`${prefix}-lable`} for={`link-desc-${editorName}`}>
-                {ult.linkModalTips?.descLable}
-              </label>
-              <input
-                placeholder={ult.linkModalTips?.descLablePlaceHolder}
-                class={`${prefix}-input`}
-                id={`link-desc-${editorName}`}
-                type="text"
-                value={linkData.desc}
-                onChange={(e) => {
-                  linkData.desc = (e.target as HTMLInputElement).value;
-                }}
-              />
-            </div>
-            <div class={`${prefix}-form-item`}>
-              <label class={`${prefix}-lable`} for={`link-url-${editorName}`}>
-                {ult.linkModalTips?.urlLable}
-              </label>
-              <input
-                placeholder={ult.linkModalTips?.UrlLablePlaceHolder}
-                class={`${prefix}-input`}
-                id={`link-url-${editorName}`}
-                type="text"
-                value={linkData.url}
-                onChange={(e) => {
-                  linkData.url = (e.target as HTMLInputElement).value;
-                }}
-              />
-            </div>
-            <div class={`${prefix}-form-item`}>
-              <button
-                class={`${prefix}-btn ${props.type === 'link' && prefix + '-btn-row'}`}
-                onClick={() => {
-                  props.onOk(linkData);
-                  linkData.desc = '';
-                  linkData.url = '';
-                }}
-              >
-                {ult.linkModalTips?.buttonOK}
-              </button>
-              {props.type === 'image' && (
-                <>
-                  <button
-                    class={`${prefix}-btn`}
-                    onClick={() => {
-                      (uploadRef.value as HTMLInputElement).click();
-                    }}
-                  >
-                    {ult.linkModalTips?.buttonUpload}
-                  </button>
-                  <input
-                    ref={uploadRef}
-                    accept="image/*"
-                    type="file"
-                    multiple="true"
-                    style={{ display: 'none' }}
-                  />
-                </>
-              )}
-            </div>
-          </>
-        )}
-      </Modal>
+      <>
+        <LinkModal
+          {...props}
+          visible={modelVisible.link}
+          onClip={() => {
+            // 关闭链接弹窗
+            modelVisible.link = false;
+            // 打开裁剪上传弹窗
+            modelVisible.clip = true;
+          }}
+        />
+        <ClipModal
+          visible={modelVisible.clip}
+          onOk={props.onOk}
+          onCancel={props.onCancel}
+          to={props.to}
+        />
+      </>
     );
   }
 });
