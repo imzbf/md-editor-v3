b02f2b00733304a683376bb521c50b11949aa08a
imzbf <zbfcqtl@163.com>
2021-08-31 14:59:36 +0800

    feat: 文档自动获取版本，新增在线编辑demo

--- dev/Doc/index.tsx
+++ dev/Doc/index.tsx
@@ -3,6 +3,8 @@ import Editor from '../../MdEditor';
 import { Theme } from '../App';
 import axios from 'axios';
 
+import { version } from '../../package.json';
+
 export default defineComponent({
   props: {
     theme: String as PropType<Theme>
@@ -14,7 +16,7 @@ export default defineComponent({
       axios
         .get('doc.md')
         .then(({ data }) => {
-          mdText.value = data;
+          mdText.value = (data as string).replaceAll('${EDITOR_VERSION}', version);
         })
         .catch((e) => {
           console.log(e);
