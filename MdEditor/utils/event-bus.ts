29929f9c4620d3794137419ac89adc0bde634f92
imzbf <zbfcqtl@163.com>
2021-09-01 18:00:57 +0800

    refactor: 以editorId命名事件池，解决多编辑器冲突问题

--- MdEditor/utils/event-bus.ts
+++ MdEditor/utils/event-bus.ts
@@ -5,35 +5,39 @@ export interface BusEvent {
 
 class Bus {
   // 事件池
-  pools: { [key: string]: Array<(p?: any) => any> } = {};
+  pools: { [race: string]: { [eventName: string]: Array<(p?: any) => any> } } = {};
 
   // 移除事件监听
-  remove(name: string, func: (p?: any) => any) {
-    const events = this.pools[name];
+  remove(race: string, name: string, func: (p?: any) => any) {
+    const targetRace = this.pools[race];
+    const events = targetRace && this.pools[race][name];
 
     if (events) {
-      this.pools[name] = events.filter((item) => item === func);
+      this.pools[race][name] = events.filter((item) => item === func);
     }
   }
 
   // 清空全部事件，由于单一实例，多次注册会被共享内容
-  clear() {
-    this.pools = {};
+  clear(race: string) {
+    this.pools[race] = {};
   }
 
   // 注册事件监听
-  on(event: BusEvent) {
-    if (this.pools[event.name] === undefined) {
-      this.pools[event.name] = [];
+  on(race: string, event: BusEvent) {
+    if (this.pools[race] === undefined) {
+      this.pools[race] = {};
+    } else if (this.pools[race][event.name] === undefined) {
+      this.pools[race][event.name] = [];
     }
 
-    this.pools[event.name].push(event.callback);
-    return this.pools[event.name].includes(event.callback);
+    this.pools[race][event.name].push(event.callback);
+    return this.pools[race][event.name].includes(event.callback);
   }
 
   // 触发事件
-  emit(name: string, ...params: any) {
-    const events = this.pools[name];
+  emit(race: string, name: string, ...params: any) {
+    const targetRace = this.pools[race];
+    const events = targetRace[name];
 
     if (events) {
       events.forEach((item) => {
