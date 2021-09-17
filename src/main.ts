e2dc3b2e02bf6f1d403537c84480ae647bf768d2
imzbf <zbfcqtl@163.com>
2021-09-17 15:48:19 +0800

    fix: 不引入全部ant样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { createApp } from 'vue';
 import App from './App';
 import router from './router';
 import 'nprogress/nprogress.css';
-import 'ant-design-vue/dist/antd.css';
 
 createApp(App).use(router).mount('#app');
