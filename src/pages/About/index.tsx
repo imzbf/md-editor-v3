f8f98230a60f6c4a3c436ad67f2ddf7570e4b740
imzbf <zbfcqtl@163.com>
2021-09-18 16:36:40 +0800

    feat: 关于页基础

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,8 +1,51 @@
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, PropType, ref } from 'vue';
+import Editor, { HeadList } from 'md-editor-v3';
+import { Theme } from '../../App';
+import axios from 'axios';
+import { version } from '../../../package.json';
+import { useStore } from 'vuex';
 
 export default defineComponent({
   name: 'PageAbout',
+  props: {
+    theme: String as PropType<Theme>
+  },
   setup() {
-    return () => <div>about</div>;
+    const mdText = ref();
+    const catalogList = ref<Array<HeadList>>([]);
+
+    onMounted(() => {
+      axios
+        .get('/about.md')
+        .then(({ data }) => {
+          mdText.value = (data as string).replace(/\$\{EDITOR_VERSION\}/g, version);
+        })
+        .catch((e) => {
+          console.log(e);
+
+          mdText.value = '文档读取失败！';
+        });
+    });
+
+    const store = useStore();
+
+    return () => (
+      <div class="container">
+        <div class="doc">
+          <div class="content" style={{ width: '100%' }}>
+            <Editor
+              theme={store.state.theme}
+              modelValue={mdText.value}
+              previewTheme={store.state.previewTheme}
+              previewOnly
+              showCodeRowNumber
+              onGetCatalog={(arr) => {
+                catalogList.value = arr;
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 });
