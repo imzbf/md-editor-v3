f01cdef8a0a3654c72295c21346470e5030ce69d
zbf <zbfcqtl@163.com>
2021-11-19 10:58:31 +0800

    refactor: 目录重构

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,26 +1,28 @@
 import { Anchor } from 'ant-design-vue';
 import { computed, defineComponent, PropType, watch } from 'vue';
 import { debounce } from '@/utils';
-import Recursive, { Head, TocItem } from './Recursive';
+import CatalogLink from './CatalogLink';
+import './style.less';
+
+export interface TocItem {
+  text: string;
+  level: number;
+  children?: Array<TocItem>;
+}
 
 const Topicfy = defineComponent({
   props: {
     // 解析得到的标题列表
     heads: {
-      type: Array as PropType<Array<Head>>
+      type: Array as PropType<Array<any>>
     }
   },
   setup(props) {
-    const topics = computed(() => {
+    const catalogs = computed(() => {
       const tocItems: TocItem[] = [];
 
-      // 标题计数器
-      // let count = 0;
-
-      const add = (text: string, level: number) => {
-        // count++;
-
-        const item = { anchor: text.replace(' ', '-'), level, text };
+      props.heads?.forEach(({ text, level }) => {
+        const item = { level, text };
 
         if (tocItems.length === 0) {
           // 第一个 item 直接 push
@@ -51,10 +53,6 @@ const Topicfy = defineComponent({
             tocItems.push(item);
           }
         }
-      };
-
-      props.heads?.forEach((item) => {
-        add(item.text.replace(/<\/?[a-z]+>/g, ''), item.level);
       });
       return tocItems;
     });
@@ -78,9 +76,9 @@ const Topicfy = defineComponent({
     watch(() => props.heads, moveToHead);
 
     return () => (
-      <Anchor affix={false} showInkInFixed={true}>
-        {topics.value.map((item) => (
-          <Recursive key={item.anchor} tocItem={item} />
+      <Anchor affix={false} showInkInFixed={false}>
+        {catalogs.value.map((item) => (
+          <CatalogLink key={item.text} tocItem={item} />
         ))}
       </Anchor>
     );
