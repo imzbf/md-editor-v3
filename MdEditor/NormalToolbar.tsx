9a5488a0dcab2a55d2b824460b39ed9410727a80
zbf <zbfcqtl@gmail.com>
2022-01-28 11:41:30 +0800

    feat: 提供NormalToolbar及DropdownToolbar两个自定义工具栏组件，提供defToolbars自定义工具栏插槽

---
+++ MdEditor/NormalToolbar.tsx
@@ -0,0 +1,32 @@
+import { defineComponent, PropType } from 'vue';
+import { prefix } from './config';
+import { getSlot } from './utils/vue-tsx';
+
+export default defineComponent({
+  name: 'NormalToolbar',
+  props: {
+    title: {
+      type: String as PropType<string>,
+      default: ''
+    },
+    // 展示在工具栏的内容，通常是个图标
+    trigger: {
+      type: [String, Object] as PropType<string | JSX.Element>
+    },
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>,
+      default: () => () => {}
+    }
+  },
+  setup(props, ctx) {
+    return () => {
+      const Trigger = getSlot({ props, ctx }, 'trigger');
+
+      return (
+        <div class={`${prefix}-toolbar-item`} title={props.title} onClick={props.onClick}>
+          <Trigger />
+        </div>
+      );
+    };
+  }
+});
--- MdEditor/index.ts
+++ MdEditor/index.ts
@@ -1,11 +1,20 @@
 import { App } from 'vue';
 
 import Editor from './Editor';
+import NormalToolbar from './NormalToolbar';
+import DropdownToolbar from './DropdownToolbar';
 
 Editor.install = (app: App) => {
   app.component(Editor.name, Editor);
+  app.component(NormalToolbar.name, NormalToolbar);
 
   return app;
 };
 
-export default Editor;
+Editor.NormalToolbar = NormalToolbar;
+Editor.DropdownToolbar = DropdownToolbar;
+
+export default Editor as typeof Editor & {
+  readonly NormalToolbar: typeof NormalToolbar;
+  readonly DropdownToolbar: typeof DropdownToolbar;
+};
