3f39be342c34c2e895862fdb2fef94e166255519
zbf <zbfcqtl@gmail.com>
2022-02-16 15:56:57 +0800

    chore: 移除开发环境无关代码

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,7 +1,6 @@
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
 import Header from './Header';
 import Preview from './Preview';
-import Doc from './Doc';
 
 import './style.less';
 
@@ -11,20 +10,11 @@ export default defineComponent({
   setup() {
     const theme = ref<Theme>('light');
 
-    onMounted(() => {
-      if (import.meta.env.MODE === 'preview') {
-        const hm = document.createElement('script');
-        hm.src = 'https://hm.baidu.com/hm.js?1563bc52cb27ffbc7b5b46cdfc327ce0';
-        document.head.appendChild(hm);
-      }
-    });
-
     return () => (
       <div class={['app', theme.value === 'dark' && 'theme-dark']}>
         <Header theme={theme.value} onChange={(v: Theme) => (theme.value = v)} />
         <div class="page-body">
           <Preview theme={theme.value} />
-          {/* <Doc theme={theme.value} /> */}
         </div>
       </div>
     );
