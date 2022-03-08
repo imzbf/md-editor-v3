477e9bfca5e1f539479e948d1ac3f0476480ea17
zbf <zbfcqtl@gmail.com>
2022-03-08 21:57:58 +0800

    refactor: 导出目录,目录组件重构

--- MdEditor/index.ts
+++ MdEditor/index.ts
@@ -3,21 +3,25 @@ import { App } from 'vue';
 import Editor from './Editor';
 import NormalToolbar from './NormalToolbar';
 import DropdownToolbar from './DropdownToolbar';
+import Catalog from './layouts/Catalog';
 
 Editor.install = (app: App) => {
   app.component(Editor.name, Editor);
   app.component(NormalToolbar.name, NormalToolbar);
   app.component(DropdownToolbar.name, DropdownToolbar);
+  app.component(Catalog.name, Catalog);
 
   return app;
 };
 
 Editor.NormalToolbar = NormalToolbar;
 Editor.DropdownToolbar = DropdownToolbar;
+Editor.Catalog = Catalog;
 
 export default Editor as typeof Editor & {
   readonly NormalToolbar: typeof NormalToolbar;
   readonly DropdownToolbar: typeof DropdownToolbar;
+  readonly Catalog: typeof Catalog;
 };
 
 export * from './type';
