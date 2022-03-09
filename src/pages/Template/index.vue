034aa5dee444512e55253e135854f038e751f51e
zbf <zbfcqtl@gmail.com>
2022-03-09 15:37:12 +0800

    feat: modal test

--- src/pages/Template/index.vue
+++ src/pages/Template/index.vue
@@ -2,9 +2,19 @@
   <md-editor
     editor-id="md1"
     v-model="text"
+    :marked-image="markedImage"
     :marked-heading="markedHeading"
     @onUploadImg="onUploadImg"
   />
+  <button @click="visible = true">打开</button>
+  <Modal v-model:visible="visible" title="Basic Modal" width="1100px">
+    <md-editor
+      editor-id="md3"
+      v-model="text2"
+      :marked-heading="markedHeading"
+      @onUploadImg="onUploadImg"
+    />
+  </Modal>
 </template>
 
 <script>
@@ -12,19 +22,24 @@ import { defineComponent } from 'vue';
 import axios from '@/utils/request';
 import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
+import { Modal } from 'ant-design-vue';
 
 export default defineComponent({
-  components: { MdEditor },
+  components: { MdEditor, Modal },
   data() {
     return {
       text: '# 123 \n哈哈哈哈\n# 456\n\n',
       text2: '# 123 \n哈哈哈哈\n# 456\n\n',
-      text3: '# 123 \n哈哈哈哈\n# 456\n\n'
+      text3: '# 123 \n哈哈哈哈\n# 456\n\n',
+      visible: false
     };
   },
   methods: {
     markedHeading(text, level, raw) {
       return `<h${level} id="${raw}" ddd>${text}</h${level}>`;
+    },
+    markedImage(href, _, desc) {
+      return `<img src="${href}" alt="${desc}">`;
     },
     onUploadImg: async (files, callback) => {
       const res = await Promise.all(
