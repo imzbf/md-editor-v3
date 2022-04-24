30619662e82a8a1e5e36e545161906ce336c1807
zbf <zbfcqtl@gmail.com>
2022-04-24 10:16:38 +0800

    fix: 扩展未加载完成对编辑器进行部分操作报错（#84）

--- MdEditor/layouts/Toolbar/composition.ts
+++ MdEditor/layouts/Toolbar/composition.ts
@@ -9,7 +9,13 @@ export const useSreenfull = (props: any) => {
   // 是否组件内部全屏标识
   const screenfullMe = ref(false);
 
+  // 触发器
   const fullScreenHandler = () => {
+    if (!screenfull) {
+      // CATCH ERROR: 捕获全局错误
+      return;
+    }
+
     if (screenfull.isEnabled) {
       screenfullMe.value = true;
       if (screenfull.isFullscreen) {
@@ -22,11 +28,8 @@ export const useSreenfull = (props: any) => {
     }
   };
 
-  const screenfullLoad = () => {
-    // 复制实例
-    screenfull = window.screenfull;
-
-    // 注册事件
+  // 挂载监听事件
+  const onScreenfullEvent = () => {
     if (screenfull && screenfull.isEnabled) {
       screenfull.on('change', () => {
         if (screenfullMe.value) {
@@ -37,15 +40,15 @@ export const useSreenfull = (props: any) => {
     }
   };
 
+  const screenfullLoad = () => {
+    // 复制实例
+    screenfull = window.screenfull;
+    // 重新监听
+    onScreenfullEvent();
+  };
+
   onMounted(() => {
-    if (screenfull && screenfull.isEnabled) {
-      screenfull.on('change', () => {
-        if (screenfullMe.value) {
-          screenfullMe.value = false;
-          props.updateSetting(!props.setting.fullscreen, 'fullscreen');
-        }
-      });
-    }
+    onScreenfullEvent();
 
     if (!previewOnly && props.screenfull === null) {
       const screenScript = document.createElement('script');
