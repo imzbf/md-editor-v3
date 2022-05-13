2dbfeac90ca2cf0c78acb39110a20cac7b30c010
zbf <zbfcqtl@gmail.com>
2022-05-13 17:21:57 +0800

    chore: nuxt example

---
+++ example/nuxt/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <md-editor v-model="text" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('# ddd');
+</script>
