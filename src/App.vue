bbe9797a3a43d583d052b4f5bdb19e5e4c78242f
zbf <zbfcqtl@gmail.com>
2022-05-20 15:06:38 +0800

    feat: 移除百度统计

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-import { onMounted } from 'vue';
-import './style.less';
-
-onMounted(() => {
-  if (import.meta.env.MODE === 'preview') {
-    const hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?1563bc52cb27ffbc7b5b46cdfc327ce0';
-    document.head.appendChild(hm);
-  }
-});
-</script>
-
 <template>
   <router-view />
 </template>
