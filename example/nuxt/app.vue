545955cc2ba07cff0fc8192fe3ac772071afbd30
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-24 22:43:31 +0800

    clean code

--- example/nuxt/app.vue
+++ example/nuxt/app.vue
@@ -1,11 +1,24 @@
 <template>
-  <md-editor v-model="text" />
+  <md-editor v-model="text" :toolbarsExclude="toolbarsExclude" />
 </template>
 
-<script setup>
+<script>
 import { ref } from 'vue';
 import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
-const text = ref('# ddd');
+export default {
+  setup() {
+    const text = ref('# Hello md-editor')
+    const toolbarsExclude = ['catalog']
+
+    return {
+      text,
+      toolbarsExclude
+    }
+  },
+  components: {
+    MdEditor
+  }
+}
 </script>
