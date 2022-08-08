d3cbc5708182db70b3df95d2406f5a15967efc79
bohecola <bohecola@outlook.com>
2022-08-08 14:30:07 +0800

    fix: modelValue length error

--- MdEditor/layouts/Footer/MarkdownTotal.tsx
+++ MdEditor/layouts/Footer/MarkdownTotal.tsx
@@ -15,7 +15,7 @@ export default defineComponent({
     return () => (
       <div class={`${prefix}-footer-item`}>
         <label class={`${prefix}-footer-label`}>{ult.value.footer?.markdownTotal}:</label>
-        <span>{props.modelValue.length}</span>
+        <span>{props.modelValue?.length || 0}</span>
       </div>
     );
   }
