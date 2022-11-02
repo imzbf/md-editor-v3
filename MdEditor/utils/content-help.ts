9e8823519dc4984d17d5238630264ca6576aa9b6
zbf <zbfcqtl@gmail.com>
2022-11-02 21:07:00 +0800

    fix: set wrong position while insert task list

--- MdEditor/utils/content-help.ts
+++ MdEditor/utils/content-help.ts
@@ -209,7 +209,6 @@ export const directive2flag = (
       case 'task': {
         targetValue = `- [ ] ${selectedText}`;
         deviationStart = 6;
-        deviationEnd = targetValue.length;
         select = true;
         break;
       }
