cb9bebb3602eba1408dd80ea0bf1c9c17afc2e48
imzbf <zbfcqtl@gmail.com>
2023-02-11 18:39:37 +0800

    chore: update example of nuxt

--- example/nuxt/app.vue
+++ example/nuxt/app.vue
@@ -7,5 +7,5 @@ import { ref } from 'vue';
 import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
-const text = ref('# ddd');
+const text = ref('# md-editor-v3');
 </script>
--- example/nuxt/nuxt.config.ts
+++ example/nuxt/nuxt.config.ts
@@ -1,6 +1,2 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-
-})
+export default defineNuxtConfig({});
