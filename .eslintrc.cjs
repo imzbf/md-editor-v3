0960254054d2b38eb166b0e2ce5663d6b661d664
zbf <zbfcqtl@gmail.com>
2023-03-15 11:05:06 +0800

    chore: eslint and prettier are valid for all files

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    'vue/prefer-import-from-vue': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off'
+  }
+};
