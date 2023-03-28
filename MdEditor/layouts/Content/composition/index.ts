046dc0010b1e7787008f498495079f67d5704e5c
imzbf <zbfcqtl@gmail.com>
2023-03-28 14:14:07 +0800

    feat: remove module useAutoGenrator, use default from codemirror

--- MdEditor/layouts/Content/composition/index.ts
+++ MdEditor/layouts/Content/composition/index.ts
@@ -1,7 +1,6 @@
 import useAutoScroll from './useAutoScroll';
 import useCodeMirror from './useCodeMirror';
 import useMarked from './useMarked';
-import useAutoGenrator from './useAutoGenrator';
 import userZoom from './userZoom';
 
-export { useAutoScroll, useCodeMirror, useMarked, useAutoGenrator, userZoom };
+export { useAutoScroll, useCodeMirror, useMarked, userZoom };
--- MdEditor/layouts/Content/index.tsx
+++ MdEditor/layouts/Content/index.tsx
@@ -1,13 +1,7 @@
 import { defineComponent, inject, ref, ComputedRef } from 'vue';
 import { PreviewThemes } from '~/type';
 import { prefix } from '~/config';
-import {
-  useAutoScroll,
-  useCodeMirror,
-  useMarked,
-  // useAutoGenrator,
-  userZoom
-} from './composition/index';
+import { useAutoScroll, useCodeMirror, useMarked, userZoom } from './composition/index';
 import { contentProps as props, ContentProps } from './props';
 
 export default defineComponent({
@@ -27,14 +21,12 @@ export default defineComponent({
     const previewRef = ref<HTMLDivElement>();
     // html代码预览框
     const htmlRef = ref<HTMLDivElement>();
-
+    //
     const { inputWrapperRef } = useCodeMirror(props);
     // markdown => html
     const html = useMarked(props);
-    // // 自动滚动
+    // 自动滚动
     useAutoScroll(props, html, '.cm-scroller', previewRef, htmlRef);
-    // 自动监听生成md内容
-    // useAutoGenrator(props);
     // 图片点击放大
     userZoom(props, html);
 
