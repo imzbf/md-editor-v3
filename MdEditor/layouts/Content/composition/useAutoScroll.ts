e14a8fedef30031d4ea6023787c843e75c3f4932
zbf <zbfcqtl@gmail.com>
2023-04-12 20:43:55 +0800

    fix: sync scrolling error while modifying content

--- MdEditor/layouts/Content/composition/useAutoScroll.ts
+++ MdEditor/layouts/Content/composition/useAutoScroll.ts
@@ -17,61 +17,7 @@ const useAutoScroll = (
 
   // 编译事件
   const rebindEvent = () => {
-    nextTick(() => {
-      // 更新完毕后判断是否需要重新绑定滚动事件
-      if (
-        (props.setting.preview || props.setting.htmlPreview) &&
-        !previewOnly &&
-        props.scrollAuto
-      ) {
-        clearScrollAuto();
-        initScrollAuto();
-      }
-    });
-  };
-
-  const settingPreviewChanged = (nVal: boolean) => {
-    // 分栏发生变化时，显示分栏时注册同步滚动，隐藏是清除同步滚动
-    if (nVal && !previewOnly) {
-      nextTick(() => {
-        clearScrollAuto();
-        const cmScroller = document.querySelector<HTMLDivElement>('.cm-scroller');
-        const scrollHandler = previewRef.value ? scrollAuto : scrollAutoWithScale;
-
-        // 需要等到页面挂载完成后再注册，否则不能正确获取到预览dom
-        [initScrollAuto, clearScrollAuto] = scrollHandler(
-          cmScroller!,
-          previewRef.value! || htmlRef.value,
-          codeMirrorUt.value!,
-          props.value
-        );
-        initScrollAuto();
-      });
-    }
-  };
-
-  watch(() => html.value, rebindEvent);
-  watch(() => props.setting.preview, settingPreviewChanged);
-  watch(() => props.setting.htmlPreview, settingPreviewChanged);
-
-  watch(
-    [toRef(props.setting, 'fullscreen'), toRef(props.setting, 'pageFullscreen')],
-    rebindEvent
-  );
-
-  // 切换滚动状态
-  watch(
-    () => props.scrollAuto,
-    (sa) => {
-      if (sa) {
-        initScrollAuto();
-      } else {
-        clearScrollAuto();
-      }
-    }
-  );
-
-  onMounted(() => {
+    clearScrollAuto();
     const cmScroller = document.querySelector<HTMLDivElement>('.cm-scroller');
 
     if (!previewOnly && (previewRef.value || htmlRef.value)) {
@@ -88,7 +34,34 @@ const useAutoScroll = (
     if (props.scrollAuto) {
       initScrollAuto();
     }
-  });
+  };
+
+  watch(
+    [
+      html,
+      toRef(props.setting, 'preview'),
+      toRef(props.setting, 'htmlPreview'),
+      toRef(props.setting, 'fullscreen'),
+      toRef(props.setting, 'pageFullscreen')
+    ],
+    () => {
+      nextTick(rebindEvent);
+    }
+  );
+
+  // 切换滚动状态
+  watch(
+    () => props.scrollAuto,
+    (sa) => {
+      if (sa) {
+        initScrollAuto();
+      } else {
+        clearScrollAuto();
+      }
+    }
+  );
+
+  onMounted(rebindEvent);
 };
 
 export default useAutoScroll;
