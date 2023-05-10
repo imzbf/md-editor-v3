1d270741364ca7ea94e3b25ed2455eccf6d16247
imzbf <zbfcqtl@gmail.com>
2023-05-10 21:52:33 +0800

    chore: nuxt example for v4

--- example/nuxt/app.vue
+++ example/nuxt/app.vue
@@ -1,10 +1,10 @@
 <template>
-  <md-editor v-model="text" />
+  <MdEditor v-model="text" />
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import MdEditor from 'md-editor-v3';
+import { MdEditor } from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
 const text = ref('# md-editor-v3');
