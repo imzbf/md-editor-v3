cc18d7dc7c7d2edd6d3883dab646d136aa196f13
zbf <zbfcqtl@gmail.com>
2024-09-03 16:20:54 +0800

    refactor: replace the syntax: `<sub>` with `~~`, and `<sup>` with `^^`

--- packages/MdEditor/layouts/Content/composition/type.d.ts
+++ packages/MdEditor/layouts/Content/composition/type.d.ts
@@ -1,4 +1,5 @@
 declare module 'markdown-it-image-figures';
 declare module 'markdown-it-task-lists';
-declare module 'markdown-it-codetabs';
 declare module 'markdown-it-xss';
+declare module 'markdown-it-sub';
+declare module 'markdown-it-sup';
