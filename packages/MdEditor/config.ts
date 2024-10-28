02eb0c5cf2a0132f9158599b82175af684a1a22d
zbf <zbfcqtl@gmail.com>
2024-10-28 15:33:36 +0800

    feat: update dependency versions, replace CDN service sources
    (cherry picked from commit afc63eea5e8d8fb5bd964875540e308aa8b4af14)

--- packages/MdEditor/config.ts
+++ packages/MdEditor/config.ts
@@ -10,23 +10,225 @@ export const defaultEditorId = 'md-editor-v3';
 export const iconfontSvgUrl = 'https://at.alicdn.com/t/c/font_2605852_cmafimm6hot.js';
 export const iconfontClassUrl = 'https://at.alicdn.com/t/c/font_2605852_cmafimm6hot.css';
 
-export const cdnBase = 'https://cdnjs.cloudflare.com/ajax/libs';
+export const cdnBase = 'https://unpkg.com';
 
 // 代码高亮cdn链接
-export const highlightUrl = `${cdnBase}/highlight.js/11.10.0/highlight.min.js`;
+export const highlightUrl = `${cdnBase}/@highlightjs/cdn-assets@11.10.0/highlight.min.js`;
 
 // 美化代码cdn连接
 export const prettierUrl = {
-  main: `${cdnBase}/prettier/2.8.0/standalone.js`,
-  markdown: `${cdnBase}/prettier/2.8.0/parser-markdown.js`
+  main: `${cdnBase}/prettier@3.3.3/standalone.js`,
+  markdown: `${cdnBase}/prettier@3.3.3/plugins/markdown.js`
 };
 
 export const cropperUrl = {
-  css: `${cdnBase}/cropperjs/1.5.13/cropper.min.css`,
-  js: `${cdnBase}/cropperjs/1.5.13/cropper.min.js`
+  css: `${cdnBase}/cropperjs@1.6.2/dist/cropper.min.css`,
+  js: `${cdnBase}/cropperjs@1.6.2/dist/cropper.min.js`
 };
 
-export const screenfullUrl = `${cdnBase}/screenfull.js/5.2.0/screenfull.min.js`;
+export const screenfullUrl = `${cdnBase}/screenfull@5.2.0/dist/screenfull.js`;
+
+export const mermaidUrl = `${cdnBase}/mermaid@11.3.0/dist/mermaid.min.js`;
+// export const mermaidUrl = `${cdnBase}/mermaid/9.4.0/mermaid.min.js`;
+
+export const katexUrl = {
+  js: `${cdnBase}/katex@0.16.11/dist/katex.min.js`,
+  css: `${cdnBase}/katex@0.16.11/dist/katex.min.css`
+};
+
+export const codeCss: CodeCss = {
+  a11y: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/a11y-light.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/a11y-dark.min.css`
+  },
+  atom: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/atom-one-light.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/atom-one-dark.min.css`
+  },
+  github: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/github.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/github-dark.min.css`
+  },
+  gradient: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/gradient-light.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/gradient-dark.min.css`
+  },
+  kimbie: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/kimbie-light.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/kimbie-dark.min.css`
+  },
+  paraiso: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/paraiso-light.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/paraiso-dark.min.css`
+  },
+  qtcreator: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/qtcreator-light.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/qtcreator-dark.min.css`
+  },
+  stackoverflow: {
+    light: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/stackoverflow-light.min.css`,
+    dark: `${cdnBase}/@highlightjs/cdn-assets@11.10.0/styles/stackoverflow-dark.min.css`
+  }
+};
+
+// 当前版本的值
+export const editorExtensionsAttrs: ConfigOption['editorExtensionsAttrs'] = {
+  highlight: {
+    js: {
+      integrity:
+        'sha384-GdEWAbCjn+ghjX0gLx7/N1hyTVmPAjdC2OvoAA0RyNcAOhqwtT8qnbCxWle2+uJX',
+      crossOrigin: 'anonymous'
+    },
+    css: {
+      a11y: {
+        light: {
+          integrity:
+            'sha384-qdZDAN3jffvh670RHw1wxLekabidEFaNRninYgIzBvMbL6WlHdXeHS/Bt+vx33lN',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-2QAAjX8pqaM5azX68KWI2wExF6Q13kY4kEiQFY4b/1zPe6rpgmTByNpDEllH3sb+',
+          crossOrigin: 'anonymous'
+        }
+      },
+      atom: {
+        light: {
+          integrity:
+            'sha384-w6Ujm1VWa9HYFqGc89oAPn/DWDi2gUamjNrq9DRvEYm2X3ClItg9Y9xs1ViVo5b5',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-oaMLBGEzBOJx3UHwac0cVndtX5fxGQIfnAeFZ35RTgqPcYlbprH9o9PUV/F8Le07',
+          crossOrigin: 'anonymous'
+        }
+      },
+      github: {
+        light: {
+          integrity:
+            'sha384-eFTL69TLRZTkNfYZOLM+G04821K1qZao/4QLJbet1pP4tcF+fdXq/9CdqAbWRl/L',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-wH75j6z1lH97ZOpMOInqhgKzFkAInZPPSPlZpYKYTOqsaizPvhQZmAtLcPKXpLyH',
+          crossOrigin: 'anonymous'
+        }
+      },
+      gradient: {
+        light: {
+          integrity:
+            'sha384-yErHBR8aEZPxRl3XmR8dGSRAclMlnSRRw8sXQLcmPWzWUvb56BzQmBw3EWHl7QGI',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-lUCvtSOdvDbp5hLWKgwz/taFu1HxlpqES2OVP5UG2JMTfnU481gXcBhGF9lAGoSr',
+          crossOrigin: 'anonymous'
+        }
+      },
+      kimbie: {
+        light: {
+          integrity:
+            'sha384-tloeSLUPczAvoZ48TUz+OxRie0oYLCRwlkadUXovGzzJEIbNQB2TkfUuvJ6SW5Mi',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-o5F1vUaMNOmou1sQrsWiFo4/QUGSV0svqNZW+EesmKxWC8MpFJcveBhAyfvTHbGb',
+          crossOrigin: 'anonymous'
+        }
+      },
+      paraiso: {
+        light: {
+          integrity:
+            'sha384-5j6QHU2Hwg1ehtlIQNDebhETDB8bga3/88hzBFsMRaGmgQHCftqIN7GZNDNw0vTL',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-I5vnnMQu0LWDQnHpT61xyoMwKarAB8jpZkB2ioFOlmzUFnIFaV4QbUwlBBOMKhTH',
+          crossOrigin: 'anonymous'
+        }
+      },
+      qtcreator: {
+        light: {
+          integrity:
+            'sha384-iEBgHrwi8Hv4dSZBz+MOGvS05rF7I7fGKM2fASQyE9jn2Istg9Qd5dSoK18WyRTB',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-D6LXJGWNR4QV7gnpuP3ccbvOYoR02td3cU0y7lESABPg/tzCSC4m+y+M2TtrmpHc',
+          crossOrigin: 'anonymous'
+        }
+      },
+      stackoverflow: {
+        light: {
+          integrity:
+            'sha384-FMwt7cTGo4aLxZnno5k0xTj0W4gmi48Kwept+y/oQmE6cFk36Kr+QJZOKNOQwORe',
+          crossOrigin: 'anonymous'
+        },
+        dark: {
+          integrity:
+            'sha384-iL+x+BroCyHm/p2c6sMA9umXhdCWp2cKe4QUjPeMzHgwXAk+ZxHyIGP3NZTZensU',
+          crossOrigin: 'anonymous'
+        }
+      }
+    }
+  },
+  prettier: {
+    standaloneJs: {
+      integrity:
+        'sha384-92h6ALm8/lHpNGn6MfGlgZ+I8c/4yn/nSN8dV9ZmDxqbP9L93gk/Jj2i0LtV+AVd',
+      crossOrigin: 'anonymous'
+    },
+    parserMarkdownJs: {
+      integrity:
+        'sha384-5ufuUgoSsr/2oihBZ5d+c+yt0qaUmzLtUz41VZNJ4txtyJ6mBve3ZwuKoq/IygYX',
+      crossOrigin: 'anonymous'
+    }
+  },
+  cropper: {
+    js: {
+      integrity:
+        'sha384-jrOgQzBlDeUNdmQn3rUt/PZD+pdcRBdWd/HWRqRo+n2OR2QtGyjSaJC0GiCeH+ir',
+      crossOrigin: 'anonymous'
+    },
+    css: {
+      integrity:
+        'sha384-6LFfkTKLRlzFtgx8xsWyBdKGpcMMQTkv+dB7rAbugeJAu1Ym2q1Aji1cjHBG12Xh',
+      crossOrigin: 'anonymous'
+    }
+  },
+  screenfull: {
+    js: {
+      integrity:
+        'sha384-Qfbv8upMDu/ikv42M0Jnym2hahbDQ77Nm8PGU0G+iA6UIwt1+scE6P1qKXA0anWU',
+      crossOrigin: 'anonymous'
+    }
+  },
+  mermaid: {
+    js: {
+      integrity:
+        'sha384-B2tp/GqmE6VfDRB3JPTsesr0+SXypThjLSvQEQH7iv3f3/PYKCm5Q4+SGPcitStz',
+      crossOrigin: 'anonymous'
+    }
+  },
+  katex: {
+    js: {
+      integrity:
+        'sha384-7zkQWkzuo3B5mTepMUcHkMB5jZaolc2xDwL6VFqjFALcbeS9Ggm/Yr2r3Dy4lfFg',
+      crossOrigin: 'anonymous'
+    },
+    css: {
+      integrity:
+        'sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+',
+      crossOrigin: 'anonymous'
+    }
+  }
+};
 
 // 所有的编辑器功能点
 export const allToolbar = [
@@ -230,216 +432,6 @@ export const staticTextDefault: StaticTextDefault = {
     footer: {
       markdownTotal: 'Character Count',
       scrollAuto: 'Scroll Auto'
-    }
-  }
-};
-
-export const mermaidUrl = `${cdnBase}/mermaid/10.6.1/mermaid.esm.min.mjs`;
-// export const mermaidUrl = `${cdnBase}/mermaid/9.4.0/mermaid.min.js`;
-
-export const katexUrl = {
-  js: `${cdnBase}/KaTeX/0.16.9/katex.min.js`,
-  css: `${cdnBase}/KaTeX/0.16.9/katex.min.css`
-};
-
-export const codeCss: CodeCss = {
-  a11y: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/a11y-light.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/a11y-dark.min.css`
-  },
-  atom: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/atom-one-light.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/atom-one-dark.min.css`
-  },
-  github: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/github.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/github-dark.min.css`
-  },
-  gradient: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/gradient-light.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/gradient-dark.min.css`
-  },
-  kimbie: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/kimbie-light.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/kimbie-dark.min.css`
-  },
-  paraiso: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/paraiso-light.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/paraiso-dark.min.css`
-  },
-  qtcreator: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/qtcreator-light.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/qtcreator-dark.min.css`
-  },
-  stackoverflow: {
-    light: `${cdnBase}/highlight.js/11.10.0/styles/stackoverflow-light.min.css`,
-    dark: `${cdnBase}/highlight.js/11.10.0/styles/stackoverflow-dark.min.css`
-  }
-};
-
-// 当前版本的值
-export const editorExtensionsAttrs: ConfigOption['editorExtensionsAttrs'] = {
-  highlight: {
-    js: {
-      integrity:
-        'sha384-GdEWAbCjn+ghjX0gLx7/N1hyTVmPAjdC2OvoAA0RyNcAOhqwtT8qnbCxWle2+uJX',
-      crossOrigin: 'anonymous'
-    },
-    css: {
-      a11y: {
-        light: {
-          integrity:
-            'sha384-qdZDAN3jffvh670RHw1wxLekabidEFaNRninYgIzBvMbL6WlHdXeHS/Bt+vx33lN',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-2QAAjX8pqaM5azX68KWI2wExF6Q13kY4kEiQFY4b/1zPe6rpgmTByNpDEllH3sb+',
-          crossOrigin: 'anonymous'
-        }
-      },
-      atom: {
-        light: {
-          integrity:
-            'sha384-w6Ujm1VWa9HYFqGc89oAPn/DWDi2gUamjNrq9DRvEYm2X3ClItg9Y9xs1ViVo5b5',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-oaMLBGEzBOJx3UHwac0cVndtX5fxGQIfnAeFZ35RTgqPcYlbprH9o9PUV/F8Le07',
-          crossOrigin: 'anonymous'
-        }
-      },
-      github: {
-        light: {
-          integrity:
-            'sha384-eFTL69TLRZTkNfYZOLM+G04821K1qZao/4QLJbet1pP4tcF+fdXq/9CdqAbWRl/L',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-wH75j6z1lH97ZOpMOInqhgKzFkAInZPPSPlZpYKYTOqsaizPvhQZmAtLcPKXpLyH',
-          crossOrigin: 'anonymous'
-        }
-      },
-      gradient: {
-        light: {
-          integrity:
-            'sha384-yErHBR8aEZPxRl3XmR8dGSRAclMlnSRRw8sXQLcmPWzWUvb56BzQmBw3EWHl7QGI',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-lUCvtSOdvDbp5hLWKgwz/taFu1HxlpqES2OVP5UG2JMTfnU481gXcBhGF9lAGoSr',
-          crossOrigin: 'anonymous'
-        }
-      },
-      kimbie: {
-        light: {
-          integrity:
-            'sha384-tloeSLUPczAvoZ48TUz+OxRie0oYLCRwlkadUXovGzzJEIbNQB2TkfUuvJ6SW5Mi',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-o5F1vUaMNOmou1sQrsWiFo4/QUGSV0svqNZW+EesmKxWC8MpFJcveBhAyfvTHbGb',
-          crossOrigin: 'anonymous'
-        }
-      },
-      paraiso: {
-        light: {
-          integrity:
-            'sha384-5j6QHU2Hwg1ehtlIQNDebhETDB8bga3/88hzBFsMRaGmgQHCftqIN7GZNDNw0vTL',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-I5vnnMQu0LWDQnHpT61xyoMwKarAB8jpZkB2ioFOlmzUFnIFaV4QbUwlBBOMKhTH',
-          crossOrigin: 'anonymous'
-        }
-      },
-      qtcreator: {
-        light: {
-          integrity:
-            'sha384-iEBgHrwi8Hv4dSZBz+MOGvS05rF7I7fGKM2fASQyE9jn2Istg9Qd5dSoK18WyRTB',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-D6LXJGWNR4QV7gnpuP3ccbvOYoR02td3cU0y7lESABPg/tzCSC4m+y+M2TtrmpHc',
-          crossOrigin: 'anonymous'
-        }
-      },
-      stackoverflow: {
-        light: {
-          integrity:
-            'sha384-FMwt7cTGo4aLxZnno5k0xTj0W4gmi48Kwept+y/oQmE6cFk36Kr+QJZOKNOQwORe',
-          crossOrigin: 'anonymous'
-        },
-        dark: {
-          integrity:
-            'sha384-iL+x+BroCyHm/p2c6sMA9umXhdCWp2cKe4QUjPeMzHgwXAk+ZxHyIGP3NZTZensU',
-          crossOrigin: 'anonymous'
-        }
-      }
-    }
-  },
-  prettier: {
-    standaloneJs: {
-      integrity:
-        'sha384-cBKW1A8TbwohtSsrm+VWpXevI1PWdPmUrozwqTw7aS51JIhz9IvgHHa/bV6icWeL',
-      crossOrigin: 'anonymous'
-    },
-    parserMarkdownJs: {
-      integrity:
-        'sha384-kXeeC2HiA6n3rpvz0MOjB7yZo9QjvYNAMihonwR5fdTOFs/rm/Bm3Xg8cOSE4900',
-      crossOrigin: 'anonymous'
-    }
-  },
-  cropper: {
-    js: {
-      integrity:
-        'sha384-r+ljwOAhwY4/kdyzMnuBg7MEVoWpTMp5EYUDntB/E9qzNwL9dAEcNrb2XaV+mJc2',
-      crossOrigin: 'anonymous'
-    },
-    css: {
-      integrity:
-        'sha384-oMy41mb/qJnpJlpXOF57hSu2KGi47l/UV9+tPNrBOs7/ap5Vubj/3phrCtjutHMQ',
-      crossOrigin: 'anonymous'
-    }
-  },
-  iconfont: {
-    integrity: 'sha384-HRrf1e8/EiqrIZQftejl7pMTeuLeYczOz7rolAGxgw86N7UjiXkex67Uf2XTZz5z',
-    crossOrigin: 'anonymous'
-  },
-  iconfontClass: {
-    integrity: 'sha384-81pjilF/gLoQvAfd8madmeylVdv8IagPQaq1C/OZbysLav7k9g+Ir3XD5qK00Q5r',
-    crossOrigin: 'anonymous'
-  },
-  screenfull: {
-    js: {
-      integrity:
-        'sha384-ELVAm9p9T4LGCG4lz8hbYalUYAppIpI1zV8IXYDCBUJFiVnV+EeG8vGSYQi+Wi4S',
-      crossOrigin: 'anonymous'
-    }
-  },
-  mermaid: {
-    js: {
-      integrity:
-        'sha384-zmipzuFBFDAekHS2uY4mN5cvpcSvgUi3aMjMJ2xnVRsU2kDHBtqLOJRnianHJeP1',
-      crossOrigin: 'anonymous'
-    }
-  },
-  katex: {
-    js: {
-      integrity:
-        'sha384-XjKyOOlGwcjNTAIQHIpgOno0Hl1YQqzUOEleOLALmuqehneUG+vnGctmUb0ZY0l8',
-      crossOrigin: 'anonymous'
-    },
-    css: {
-      integrity:
-        'sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV',
-      crossOrigin: 'anonymous'
     }
   }
 };
--- packages/MdEditor/utils/dom.ts
+++ packages/MdEditor/utils/dom.ts
@@ -143,83 +143,95 @@ export const zoomMermaid = (() => {
     };
 
     // 处理拖拽和单指移动
-    container.addEventListener('touchstart', (event) => {
-      if (event.touches.length === 1) {
-        isDragging = true;
-        startX = event.touches[0].clientX - posX;
-        startY = event.touches[0].clientY - posY;
-      } else if (event.touches.length === 2) {
-        initialDistance = Math.hypot(
-          event.touches[0].clientX - event.touches[1].clientX,
-          event.touches[0].clientY - event.touches[1].clientY
-        );
-        initialScale = scale;
-      }
-    });
+    container.addEventListener(
+      'touchstart',
+      (event) => {
+        if (event.touches.length === 1) {
+          isDragging = true;
+          startX = event.touches[0].clientX - posX;
+          startY = event.touches[0].clientY - posY;
+        } else if (event.touches.length === 2) {
+          initialDistance = Math.hypot(
+            event.touches[0].clientX - event.touches[1].clientX,
+            event.touches[0].clientY - event.touches[1].clientY
+          );
+          initialScale = scale;
+        }
+      },
+      { passive: false }
+    );
 
-    container.addEventListener('touchmove', (event) => {
-      event.preventDefault();
+    container.addEventListener(
+      'touchmove',
+      (event) => {
+        event.preventDefault();
 
-      if (isDragging && event.touches.length === 1) {
-        posX = event.touches[0].clientX - startX;
-        posY = event.touches[0].clientY - startY;
-        updateTransform();
-      } else if (event.touches.length === 2) {
-        const newDistance = Math.hypot(
-          event.touches[0].clientX - event.touches[1].clientX,
-          event.touches[0].clientY - event.touches[1].clientY
-        );
-        const scaleChange = newDistance / initialDistance;
-        const previousScale = scale;
-        scale = initialScale * (1 + (scaleChange - 1)); // 调整缩放速度
+        if (isDragging && event.touches.length === 1) {
+          posX = event.touches[0].clientX - startX;
+          posY = event.touches[0].clientY - startY;
+          updateTransform();
+        } else if (event.touches.length === 2) {
+          const newDistance = Math.hypot(
+            event.touches[0].clientX - event.touches[1].clientX,
+            event.touches[0].clientY - event.touches[1].clientY
+          );
+          const scaleChange = newDistance / initialDistance;
+          const previousScale = scale;
+          scale = initialScale * (1 + (scaleChange - 1)); // 调整缩放速度
 
-        // 计算双指中心点
-        const centerX = (event.touches[0].clientX + event.touches[1].clientX) / 2;
-        const centerY = (event.touches[0].clientY + event.touches[1].clientY) / 2;
+          // 计算双指中心点
+          const centerX = (event.touches[0].clientX + event.touches[1].clientX) / 2;
+          const centerY = (event.touches[0].clientY + event.touches[1].clientY) / 2;
 
-        // 获取内容区域的边界
-        const rect = content.getBoundingClientRect();
-        // 计算相对位置
-        const relativeX = (centerX - rect.left) / previousScale;
-        const relativeY = (centerY - rect.top) / previousScale;
+          // 获取内容区域的边界
+          const rect = content.getBoundingClientRect();
+          // 计算相对位置
+          const relativeX = (centerX - rect.left) / previousScale;
+          const relativeY = (centerY - rect.top) / previousScale;
 
-        // 调整 posX 和 posY 使得缩放发生在双指中心
-        posX -= relativeX * (scale - previousScale);
-        posY -= relativeY * (scale - previousScale);
+          // 调整 posX 和 posY 使得缩放发生在双指中心
+          posX -= relativeX * (scale - previousScale);
+          posY -= relativeY * (scale - previousScale);
 
-        updateTransform();
-      }
-    });
+          updateTransform();
+        }
+      },
+      { passive: false }
+    );
 
     container.addEventListener('touchend', () => {
       isDragging = false;
     });
 
     // PC 端缩放功能
-    container.addEventListener('wheel', (event) => {
-      event.preventDefault();
-      const scaleAmount = 0.02; // 缩放速度
-      const previousScale = scale;
+    container.addEventListener(
+      'wheel',
+      (event) => {
+        event.preventDefault();
+        const scaleAmount = 0.02; // 缩放速度
+        const previousScale = scale;
 
-      if (event.deltaY < 0) {
-        // 放大
-        scale += scaleAmount;
-      } else {
-        // 缩小
-        scale = Math.max(0.1, scale - scaleAmount);
-      }
+        if (event.deltaY < 0) {
+          // 放大
+          scale += scaleAmount;
+        } else {
+          // 缩小
+          scale = Math.max(0.1, scale - scaleAmount);
+        }
 
-      // 计算鼠标相对于内容的位置
-      const rect = content.getBoundingClientRect();
-      const mouseX = event.clientX - rect.left;
-      const mouseY = event.clientY - rect.top;
+        // 计算鼠标相对于内容的位置
+        const rect = content.getBoundingClientRect();
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
 
-      // 调整 posX 和 posY，以使缩放中心为鼠标位置
-      posX -= (mouseX / previousScale) * (scale - previousScale);
-      posY -= (mouseY / previousScale) * (scale - previousScale);
+        // 调整 posX 和 posY，以使缩放中心为鼠标位置
+        posX -= (mouseX / previousScale) * (scale - previousScale);
+        posY -= (mouseY / previousScale) * (scale - previousScale);
 
-      updateTransform();
-    });
+        updateTransform();
+      },
+      { passive: false }
+    );
 
     // PC 端拖拽功能
     container.addEventListener('mousedown', (event) => {
