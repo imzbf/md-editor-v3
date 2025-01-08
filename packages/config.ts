be23c712abd5e458ed351dafa1def746a4ec02c7
zbf <zbfcqtl@gmail.com>
2025-01-08 15:25:51 +0800

    feat: export `prefix`

--- packages/config.ts
+++ packages/config.ts
@@ -1,6 +1,6 @@
 import { staticTextDefault } from '~/config';
 
-export { config, allToolbar, allFooter, editorExtensionsAttrs } from '~/config';
+export { prefix, config, allToolbar, allFooter, editorExtensionsAttrs } from '~/config';
 
 export const zh_CN = staticTextDefault['zh-CN'];
 export const en_US = staticTextDefault['en-US'];
