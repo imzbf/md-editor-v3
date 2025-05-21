ccc942f6d569290d0ba5a5ee010e1721e7c88f50
imzbf <zbfcqtl@gmail.com>
2025-05-21 18:01:04 +0800

    feat: `extraTools` support is set to function

--- packages/MdEditor/layouts/Content/markdownIt/code/index.ts
+++ packages/MdEditor/layouts/Content/markdownIt/code/index.ts
@@ -19,7 +19,7 @@ export interface CodeTabsPluginOps extends markdownit.Options {
   codeFoldable: boolean;
   autoFoldThreshold: number;
   customIconRef: ComputedRef<CustomIcon>;
-  extraTools?: string;
+  extraTools?: string | (({ lang }: { lang: string }) => string);
 }
 
 const codetabs = (md: markdownit, _opts: CodeTabsPluginOps) => {
@@ -96,7 +96,7 @@ const codetabs = (md: markdownit, _opts: CodeTabsPluginOps) => {
             <div class="${prefix}-code-action">
               <span class="${prefix}-code-lang">${tokens[idx].info.trim()}</span>
               <span class="${prefix}-copy-button" data-tips="${codeCodeText}"${isIcon ? ' data-is-icon=true' : ''}>${copyBtnHtml}</span>
-              ${_opts.extraTools || ''}
+              ${_opts.extraTools instanceof Function ? _opts.extraTools({ lang: tokens[idx].info.trim() }) : _opts.extraTools || ''}
               ${tagContainer === 'details' ? collapseTips : ''}
             </div>
           </${tagHeader}>
