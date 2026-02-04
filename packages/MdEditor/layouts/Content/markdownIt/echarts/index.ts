cfaa212d930908093b344f216a4db8c6d916a37f
beginor <beginor@qq.com>
2026-02-04 17:57:53 +0800

    Enhance EchartsPlugin with dynamic sizing
    Refactor EchartsPlugin to dynamically set width and height based on chart configuration.

--- packages/MdEditor/layouts/Content/markdownIt/echarts/index.ts
+++ packages/MdEditor/layouts/Content/markdownIt/echarts/index.ts
@@ -7,7 +7,7 @@ const EchartsPlugin = (md: markdownit, options: { themeRef: ComputedRef<Themes> 
   const temp = md.renderer.rules.fence!.bind(md.renderer.rules);
   md.renderer.rules.fence = (tokens, idx, ops, env, slf) => {
     const token = tokens[idx];
-    const code = token.content.trim();
+    let code = token.content.trim();
     if (token.info === 'echarts') {
       token.attrSet('class', `${prefix}-echarts`);
       token.attrSet('data-echarts-theme', options.themeRef.value);
@@ -21,7 +21,29 @@ const EchartsPlugin = (md: markdownit, options: { themeRef: ComputedRef<Themes> 
         token.attrSet('data-line', String(token.map[0]));
       }
 
-      return `<div ${slf.renderAttrs(token)} style="width: 100%; aspect-ratio: 4 / 3;">${md.utils.escapeHtml(code)}</div>`;
+      let width = 640;
+      let height = 480;
+
+      try {
+        const func = new Function('return (' + code + ')')
+        const cfg = func();
+        if (cfg.grid) {
+          const w = cfg.grid.width;
+          const h = cfg.grid.height;
+          if (!!w && !!h) {
+            width = w;
+            height = h;
+          }
+          delete cfg.grid;
+          code = JSON.stringify(cfg);
+        }
+
+      }
+      catch (ex: any) {
+        console.error(ex);
+      }
+
+      return `<div ${slf.renderAttrs(token)} style="margin: 0 auto; width: ${width}px; height: ${height}px;">${md.utils.escapeHtml(code)}</div>`;
     }
 
     return temp(tokens, idx, ops, env, slf);
